b9192f841184a564aa9fe8eecdb57f01db8fb863
Sushil Choudhary <sushil.choudhary@cloudsherpas.com>
2016-11-09 14:02:17 +0000

    Addition of rx dependencies and java class

---
+++ src/main/java/com/hmrc/shoppping/basket/BasketPriceCalculator.java
@@ -0,0 +1,18 @@
+package com.hmrc.shoppping.basket;
+
+import java.util.List;
+
+import rx.Observable;
+
+public class BasketPriceCalculator {
+
+	public BasketPriceCalculator(List<String> basket) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Observable totalActualPrice() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
