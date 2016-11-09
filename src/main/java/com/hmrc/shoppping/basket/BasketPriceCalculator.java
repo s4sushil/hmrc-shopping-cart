4196fd02fa771d6127488b9b1136ef53e16321b5
Sushil Choudhary <sushil.choudhary@cloudsherpas.com>
2016-11-09 14:26:09 +0000

    deleted unwanted enum and added test for offers and promotions

--- src/main/java/com/hmrc/shoppping/basket/BasketPriceCalculator.java
+++ src/main/java/com/hmrc/shoppping/basket/BasketPriceCalculator.java
@@ -15,16 +15,23 @@ import rx.Observable;
  */
 public class BasketPriceCalculator {
 
-	private Observable<Integer> totalPriceObservable;
 	private Observable<String> basketObservable;
+	private Observable<Integer> totalPriceObservable;
+	private Observable<Integer> totalPromotionalPriceObservable;
 
 	public BasketPriceCalculator(List<String> basketItems) {
 		basketObservable = Observable.from(basketItems);
 		totalPriceObservable = Observable.merge(getApplePrice(), getOrangePrice()).scan(0, this::sum);
+		totalPromotionalPriceObservable = Observable.merge(getPromotionalApplePrice(), getPromotionalOrangePrice())
+				.scan(0, this::sum);
 	}
 
 	public Observable<Integer> totalActualPrice() {
 		return totalPriceObservable;
+	}
+
+	public Observable<Integer> totalPromotionalActualPrice() {
+		return totalPromotionalPriceObservable;
 	}
 
 	private Observable<Integer> getApplePrice() {
@@ -33,6 +40,15 @@ public class BasketPriceCalculator {
 
 	private Observable<Integer> getOrangePrice() {
 		return basketObservable.filter(ItemEnum.ORANGE.name()::equals).map((eachOffer) -> PriceConstants.ORANGE_RATE);
+	}
+
+	private Observable<Integer> getPromotionalApplePrice() {
+		return basketObservable.filter(ItemEnum.APPLE.name()::equals).buffer(2).map((eachOffer) -> PriceConstants.APPLE_RATE);
+	}
+
+	private Observable<Integer> getPromotionalOrangePrice() {
+		return basketObservable.filter(ItemEnum.ORANGE.name()::equals).buffer(3)
+				.map((eachOffer) -> eachOffer.size() == 3 ? PriceConstants.ORANGE_RATE * 2 : PriceConstants.ORANGE_RATE * eachOffer.size());
 	}
 
 	private Integer sum(Integer acc, Integer next) {
--- src/test/java/com/hmrc/shoppping/basket/BasketPriceCalculatorTest.java
+++ src/test/java/com/hmrc/shoppping/basket/BasketPriceCalculatorTest.java
@@ -39,10 +39,10 @@ public class BasketPriceCalculatorTest {
 
 	@Test
 	public void testActualPriceOfBasketOfMultipleItems() {
-		List<String> basket = Arrays.asList(ItemEnum.ORANGE.name(), ItemEnum.ORANGE.name(), ItemEnum.ORANGE.name(),
-				ItemEnum.ORANGE.name(), ItemEnum.APPLE.name(), ItemEnum.APPLE.name(), ItemEnum.APPLE.name());
+		List<String> basket = Arrays.asList(ItemEnum.ORANGE.name(), ItemEnum.ORANGE.name(), ItemEnum.APPLE.name(),
+				ItemEnum.APPLE.name());
 		BasketPriceCalculator calc = new BasketPriceCalculator(basket);
-		assertThat(BlockingObservable.from(calc.totalActualPrice()).last(), is(280));
+		assertThat(BlockingObservable.from(calc.totalActualPrice()).last(), is(170));
 	}
 
 	@Test
@@ -51,6 +51,29 @@ public class BasketPriceCalculatorTest {
 				ItemEnum.ORANGE.name(), ItemEnum.APPLE.name(), ItemEnum.APPLE.name(), ItemEnum.APPLE.name());
 		BasketPriceCalculator calc = new BasketPriceCalculator(basket);
 		assertThat(BlockingObservable.from(calc.totalActualPrice()).last(), is(280));
+	}
+	
+	@Test
+	public void testPromotionalPriceOfEmptyItemsInBasket() {
+		List<String> basket = Arrays.asList();
+		BasketPriceCalculator calc = new BasketPriceCalculator(basket);
+		assertThat(BlockingObservable.from(calc.totalPromotionalActualPrice()).last(), is(0));
+	}
+
+	@Test
+	public void testPromotionalPriceOfBasketFullOfApples() {
+		List<String> basketOfApples = Arrays.asList(ItemEnum.APPLE.name(), ItemEnum.APPLE.name(),
+				ItemEnum.APPLE.name());
+		BasketPriceCalculator calc = new BasketPriceCalculator(basketOfApples);
+		assertThat(BlockingObservable.from(calc.totalPromotionalActualPrice()).last(), is(120));
+	}
+
+	@Test
+	public void testPromotionalPriceOfBasketFullOfOranges() {
+		List<String> basketOfOranges = Arrays.asList(ItemEnum.ORANGE.name(), ItemEnum.ORANGE.name(),
+				ItemEnum.ORANGE.name());
+		BasketPriceCalculator calc = new BasketPriceCalculator(basketOfOranges);
+		assertThat(BlockingObservable.from(calc.totalPromotionalActualPrice()).last(), is(50));
 	}
 
 }
